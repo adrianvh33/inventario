0318691df06509d76044bf51d1d1e94b30dcbfe3
adrianvh33 <adrian.valencia02@usc.edu.co>
2022-03-25 14:09:51 -0500

    Se agrega las vistas de los lectores

--- lector/urls.py
+++ lector/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('leer', views.leer,name='lector.leer'),
+    path('lector', views.leerList,name='lector.list'),
+    path('lector/leerTienda', views.leerTienda,name='lector.leerTienda'),
+    path('lector/leerBloque2', views.leerBloque2,name='lector.leerBloque2'),
+    path('lector/leerBloque5', views.leerBloque5,name='lector.leerBloque5'),
 ]
