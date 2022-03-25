e7e6487b4eeced319af6b1485c1a5075b234d824
adrianvh33 <adrian.valencia02@usc.edu.co>
2022-03-24 20:43:23 -0500

    Se vista de lector

--- lector/views.py
+++ lector/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from .forms import LeerForm
+from producto.models import Producto
+from django.http import Http404,HttpResponseRedirect,HttpResponse
+from django.urls import reverse
 # Create your views here.
 def leer(request):
     if request.method == 'POST':
         filled_form = LeerForm(request.POST)
         if filled_form.is_valid():
-            referencia = filled_form.referencia
+            referencia = filled_form.cleaned_data['referencia']
             print(referencia)
+            if Producto.objects.filter(referencia=referencia).exists():
+                prod = Producto.objects.get(referencia=referencia)
+                can = prod.cantidadContada +1
+                print(can)
+                prod.cantidadContada = can
+                prod.save()
+            note = 'Cantidad contada de {} - {} es {}'.format(prod.referencia,prod.nombre,prod.cantidadContada)    
             form = LeerForm()
+            return render(request,'lector/leer.html', {'form':form, 'note':note})
     else:
         form = LeerForm()
-    return render(request,'lector/leer.html', {'form':form})
+        return render(request,'lector/leer.html', {'form':form})
